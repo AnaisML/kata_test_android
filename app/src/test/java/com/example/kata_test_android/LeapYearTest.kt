528ade94846b565d696b5c3d30e7ef58a53db6ed
Anais Troncoso <anais.troncoso@mercadolibre.cl>
2021-01-05 14:11:05 -0300

    description

---
+++ app/src/test/java/com/example/kata_test_android/LeapYearTest.kt
@@ -0,0 +1,18 @@
+package com.example.kata_test_android
+
+import org.junit.Assert.*
+import org.junit.Test
+
+/*
+* 2004, 2008, 2012, 2016, 2020, 2024, 2028, 2032, 2036,
+* 2040, 2044, 2048, 2052, 2056, 2060, 2064, 2068, 2072,
+* 2076, 2080, 2084, 2088, 2092, 2096.
+* */
+
+class LeapYearTest {
+
+    @Test
+    fun isLeapYearTest() {
+
+    }
+}
