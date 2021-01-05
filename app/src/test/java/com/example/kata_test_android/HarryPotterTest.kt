d26416a88b520dd598cfc5433d7509eb215a6662
Anais Troncoso <anais.troncoso@mercadolibre.cl>
2021-01-05 14:40:58 -0300

    exercise

---
+++ app/src/test/java/com/example/kata_test_android/HarryPotterTest.kt
@@ -0,0 +1,13 @@
+package com.example.kata_test_android
+
+import org.junit.Assert
+import org.junit.Test
+
+class HarryPotterTest {
+
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+
+}
